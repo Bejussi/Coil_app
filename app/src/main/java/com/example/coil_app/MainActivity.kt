69cbd0cb58f4e8d4ecd5cf42eafa71a3c7c3ec86
Alexandra <pindooqy@gmail.com>
2022-05-13 14:03:35 +0300

    Load image from url

--- app/src/main/java/com/example/coil_app/MainActivity.kt
+++ app/src/main/java/com/example/coil_app/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.coil_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import coil.load
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        switch_image.setOnCheckedChangeListener { _, b ->
+            if (b) {
+                Toast.makeText(this,"First image load",Toast.LENGTH_LONG).show()
+                imageView.load("https://upload.wikimedia.org/wikipedia/uk/1/18/Cuphead_icon.jpg")
+            }
+            else {
+                Toast.makeText(this,"Second image load",Toast.LENGTH_LONG).show()
+                imageView.load("https://bonduelle.ru/upload/medialibrary/81f/81fb1f09e1159ffcf8a6f89a3848bfee.jpg")
+            }
+        }
     }
 }
